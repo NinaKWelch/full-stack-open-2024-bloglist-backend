dbc59007fe1eeb5b81d62509151c2c21e3571673
NinaKWelch <ninakwelch@gmail.com>
2025-01-24 17:08:29 +0000

    exercise 4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,10 +1,32 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'First Blog',
+    author: 'John',
+    url: 'http://john.com',
+  },
+  {
+    title: 'Second Blog',
+    author: 'Jill',
+    url: 'http://jill.com',
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -17,6 +39,12 @@ test('there are two blogs', async () => {
   const response = await api.get('/api/blogs')
   
   assert.strictEqual(response.body.length, 2)
+})
+
+test('blog has an id property', async () => {
+  const response = await api.get('/api/blogs')
+  
+  assert(response.body[0].id)
 })
 
 after(async () => {
