f9400fa7c59bcb158292ae1923b379f812f528c9
NinaKWelch <ninakwelch@gmail.com>
2025-01-24 15:56:15 +0000

    refactors app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # full-stack-open-2024-bloglist-backend
+
 Blog List backend application
+
+https://enigmatic-plains-03238-5cf357ba415b.herokuapp.com
--- models/blog.js
+++ models/blog.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose')
+const config = require('../utils/config')
+const logger = require('../utils/logger')
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
 
 const blogSchema = new mongoose.Schema({
   title: String,
